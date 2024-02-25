4cdb3e7d5781abd94c0601b68a0f01a635a17410
Dreadyyyy <90562473+Dreadyyyy@users.noreply.github.com>
2024-02-25 23:44:57 +0100

    Fix
    Fixed layout for unfolded foldable

--- app/src/main/java/com/example/basiccalculatorapp/ui/screens/content/ButtonsPanes.kt
+++ app/src/main/java/com/example/basiccalculatorapp/ui/screens/content/ButtonsPanes.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.basiccalculatorapp.data.CalculatorButton
 import com.example.basiccalculatorapp.ui.utils.ButtonsFontSize
+import com.example.compose.BasicCalculatorAppTheme
 
 @Composable
 fun CompactButtonsPane(
@@ -95,6 +96,104 @@ fun CompactButtonsPane(
                 CalculatorButton.Zero to basicButtonModifier,
                 CalculatorButton.Comma to basicButtonModifier,
                 CalculatorButton.Equals to basicButtonModifier,
+            ),
+            modifier = Modifier
+                .weight(1F)
+                .fillMaxHeight()
+        )
+    }
+}
+
+@Composable
+fun MediumButtonsPane(
+    buttonsFontSize: ButtonsFontSize,
+    onClickAction: (CalculatorButton) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        val basicButtonModifier: Modifier = Modifier
+            .clip(CircleShape)
+            .fillMaxHeight()
+            .padding(4.dp)
+            .weight(1F)
+        val biggerButtonModifier: Modifier = Modifier
+            .clip(CircleShape)
+            .fillMaxHeight()
+            .padding(4.dp)
+            .weight(2F)
+        RowOfButtons(
+            buttonsFontSize = buttonsFontSize,
+            onClickAction = onClickAction,
+            buttons = listOf(
+                CalculatorButton.Clear to biggerButtonModifier,
+                CalculatorButton.OpeningBracket to basicButtonModifier,
+                CalculatorButton.ClosingBracket to basicButtonModifier,
+                CalculatorButton.E to basicButtonModifier,
+                CalculatorButton.Pi to basicButtonModifier
+            ),
+            modifier = Modifier
+                .weight(1F)
+                .fillMaxHeight()
+        )
+        RowOfButtons(
+            buttonsFontSize = buttonsFontSize,
+            onClickAction = onClickAction,
+            buttons = listOf(
+                CalculatorButton.Sin to basicButtonModifier,
+                CalculatorButton.Cos to basicButtonModifier,
+                CalculatorButton.One to basicButtonModifier,
+                CalculatorButton.Two to basicButtonModifier,
+                CalculatorButton.Three to basicButtonModifier,
+                CalculatorButton.Plus to basicButtonModifier
+            ),
+            modifier = Modifier
+                .weight(1F)
+                .fillMaxHeight()
+        )
+        RowOfButtons(
+            buttonsFontSize = buttonsFontSize,
+            onClickAction = onClickAction,
+            buttons = listOf(
+                CalculatorButton.Tan to basicButtonModifier,
+                CalculatorButton.PowerOf to basicButtonModifier,
+                CalculatorButton.Four to basicButtonModifier,
+                CalculatorButton.Five to basicButtonModifier,
+                CalculatorButton.Six to basicButtonModifier,
+                CalculatorButton.Minus to basicButtonModifier
+            ),
+            modifier = Modifier
+                .weight(1F)
+                .fillMaxHeight()
+        )
+        RowOfButtons(
+            buttonsFontSize = buttonsFontSize,
+            onClickAction = onClickAction,
+            buttons = listOf(
+                CalculatorButton.Sqrt to basicButtonModifier,
+                CalculatorButton.Log to basicButtonModifier,
+                CalculatorButton.Seven to basicButtonModifier,
+                CalculatorButton.Eight to basicButtonModifier,
+                CalculatorButton.Nine to basicButtonModifier,
+                CalculatorButton.Multiply to basicButtonModifier
+            ),
+            modifier = Modifier
+                .weight(1F)
+                .fillMaxHeight()
+        )
+        RowOfButtons(
+            buttonsFontSize = buttonsFontSize,
+            onClickAction = onClickAction,
+            buttons = listOf(
+                CalculatorButton.Ln to basicButtonModifier,
+                CalculatorButton.Factorial to basicButtonModifier,
+                CalculatorButton.Comma to basicButtonModifier,
+                CalculatorButton.Zero to biggerButtonModifier,
+                CalculatorButton.Period to basicButtonModifier,
+                CalculatorButton.Divide to basicButtonModifier
             ),
             modifier = Modifier
                 .weight(1F)
@@ -193,101 +292,7 @@ fun ExpandedButtonsPane(
     }
 }
 
-@Composable
-fun MediumButtonsPane(
-    buttonsFontSize: ButtonsFontSize,
-    onClickAction: (CalculatorButton) -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Column(
-        verticalArrangement = Arrangement.Bottom,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier
-    ) {
-        val basicButtonModifier: Modifier = Modifier
-            .clip(CircleShape)
-            .fillMaxHeight()
-            .padding(4.dp)
-            .weight(1F)
-        val biggerButtonModifier: Modifier = Modifier
-            .clip(CircleShape)
-            .fillMaxHeight()
-            .padding(4.dp)
-            .weight(2F)
-        RowOfButtons(
-            buttonsFontSize = buttonsFontSize,
-            onClickAction = onClickAction,
-            buttons = listOf(
-                CalculatorButton.Clear to biggerButtonModifier,
-                CalculatorButton.OpeningBracket to biggerButtonModifier,
-                CalculatorButton.ClosingBracket to biggerButtonModifier
-            ),
-            modifier = Modifier
-                .weight(1F)
-                .fillMaxHeight()
-        )
-        RowOfButtons(
-            buttonsFontSize = buttonsFontSize,
-            onClickAction = onClickAction,
-            buttons = listOf(
-                CalculatorButton.Sin to basicButtonModifier,
-                CalculatorButton.Cos to basicButtonModifier,
-                CalculatorButton.One to basicButtonModifier,
-                CalculatorButton.Two to basicButtonModifier,
-                CalculatorButton.Three to basicButtonModifier,
-                CalculatorButton.Plus to basicButtonModifier
-            ),
-            modifier = Modifier
-                .weight(1F)
-                .fillMaxHeight()
-        )
-        RowOfButtons(
-            buttonsFontSize = buttonsFontSize,
-            onClickAction = onClickAction,
-            buttons = listOf(
-                CalculatorButton.Tan to basicButtonModifier,
-                CalculatorButton.PowerOf to basicButtonModifier,
-                CalculatorButton.Four to basicButtonModifier,
-                CalculatorButton.Five to basicButtonModifier,
-                CalculatorButton.Six to basicButtonModifier,
-                CalculatorButton.Minus to basicButtonModifier
-            ),
-            modifier = Modifier
-                .weight(1F)
-                .fillMaxHeight()
-        )
-        RowOfButtons(
-            buttonsFontSize = buttonsFontSize,
-            onClickAction = onClickAction,
-            buttons = listOf(
-                CalculatorButton.PowerOf to basicButtonModifier,
-                CalculatorButton.Log to basicButtonModifier,
-                CalculatorButton.Seven to basicButtonModifier,
-                CalculatorButton.Eight to basicButtonModifier,
-                CalculatorButton.Nine to basicButtonModifier,
-                CalculatorButton.Multiply to basicButtonModifier
-            ),
-            modifier = Modifier
-                .weight(1F)
-                .fillMaxHeight()
-        )
-        RowOfButtons(
-            buttonsFontSize = buttonsFontSize,
-            onClickAction = onClickAction,
-            buttons = listOf(
-                CalculatorButton.Ln to basicButtonModifier,
-                CalculatorButton.Factorial to basicButtonModifier,
-                CalculatorButton.Comma to basicButtonModifier,
-                CalculatorButton.Zero to biggerButtonModifier,
-                CalculatorButton.Period to basicButtonModifier,
-                CalculatorButton.Divide to basicButtonModifier
-            ),
-            modifier = Modifier
-                .weight(1F)
-                .fillMaxHeight()
-        )
-    }
-}
+
 @Composable
 fun RowOfButtons(
     buttonsFontSize: ButtonsFontSize,
@@ -300,14 +305,14 @@ fun RowOfButtons(
         horizontalArrangement = Arrangement.Center,
         modifier = modifier.fillMaxWidth()
     ) {
-        buttons.forEach { (button: CalculatorButton, localModifier: Modifier)->
+        buttons.forEach { (button: CalculatorButton, localModifier: Modifier) ->
             Button(
                 onClick = { onClickAction(button) },
                 modifier = localModifier
             ) {
                 Text(
                     text = button.calculatorButtonType.buttonText,
-                    fontSize = when(buttonsFontSize) {
+                    fontSize = when (buttonsFontSize) {
                         ButtonsFontSize.Compact -> 16.sp
                         ButtonsFontSize.Medium -> 20.sp
                         ButtonsFontSize.Expanded -> 32.sp
@@ -321,19 +326,36 @@ fun RowOfButtons(
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun PrimaryButtonsPanePreview() {
-    CompactButtonsPane(
-        buttonsFontSize = ButtonsFontSize.Expanded,
-        onClickAction = {_->},
-        modifier = Modifier.fillMaxWidth()
-    )
+fun CompactButtonsPanePreview() {
+    BasicCalculatorAppTheme {
+        CompactButtonsPane(
+            buttonsFontSize = ButtonsFontSize.Compact,
+            onClickAction = { _ -> },
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
 }
+
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun SecondaryButtonsPanePreview() {
-    ExpandedButtonsPane(
-        buttonsFontSize = ButtonsFontSize.Expanded,
-        onClickAction = {_->},
-        modifier = Modifier.fillMaxWidth()
-    )
+fun MediumButtonsPanePreview() {
+    BasicCalculatorAppTheme {
+        MediumButtonsPane(
+            buttonsFontSize = ButtonsFontSize.Medium,
+            onClickAction = {},
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true, widthDp = 1000)
+@Composable
+fun ExpandedButtonsPanePreview() {
+    BasicCalculatorAppTheme {
+        ExpandedButtonsPane(
+            buttonsFontSize = ButtonsFontSize.Expanded,
+            onClickAction = { _ -> },
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
 }
